a600781d30132bfe1095be33ea7f4808719260ea
NICOLAS CASTRO JARAMILLO <nicolas.castro-j@mail.escuelaing.edu.co>
2024-02-02 07:19:13 -0500

    primer punto

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.threads;
 
 /**
  *
- * @author hcadavid
+ * @author Nicolás Castro, Hann Jang, Jose Samuel, Juan Pablo
  */
-public class CountThread {
-    
+
+/**
+ * Clase que representa un hilo que cuenta desde un número A hasta un número B.
+ */
+public class CountThread extends Thread{
+
+    private int numA;
+
+    private int numB;
+
+ 
+    /**
+     * Constructor de la clase CountThread.
+     *
+     * @param numA El número inicial del contador.
+     * @param numB El número final del contador.
+     * @param name El nombre del hilo.
+     */
+    public CountThread(int numA, int numB, String name){
+        // llamar al constructor de la superclase Thread
+        super(name);
+        this.numA = numA;
+        this.numB = numB;
+
+    }
+
+   
+    /**
+     * Método principal que ejecuta el hilo. Cuenta desde numA hasta numB e imprime información en la consola.
+     */
+     public void run(){
+
+        for (int i = numA; i < numB ; i++)
+        System.out.println(i + " " + getName());
+        System.out.println("Termina thread " + getName());
+
+    }
 }
+
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -1,18 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.threads;
 
 /**
  *
- * @author hcadavid
+ * @author Nicolás Castro, Hann Jang, Jose Samuel, Juan Pablo
+ */
+
+/**
+ * Clase principal que demuestra el uso de hilos para contar en paralelo.
  */
 public class CountThreadsMain {
-    
-    public static void main(String a[]){
-        
+
+
+    /**
+     * Método principal.
+     *
+     * @param args  argumentos de la línea de comandos.
+     */
+    public static void main(String args[]){
+
+        Thread hilo1 = new Thread(new CountThread(0,100,"hilo1"));
+        Thread hilo2 = new Thread(new CountThread(99,200,"hilo2"));
+        Thread hilo3 = new Thread(new CountThread(199,300,"hilo3"));
+
+ 
+
+        // hilo1.start();
+        // hilo2.start();
+        // hilo3.start();
+
+ 
+
+        hilo1.run();
+        hilo2.run();
+        hilo3.run();
     }
     
 }
+
